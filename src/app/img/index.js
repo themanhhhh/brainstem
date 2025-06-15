8445c64378793dbe0898b17244ea3ce8e43be2b6
TaTien <tien.tn.2280@aptechlearning.edu.vn>
2025-06-15 11:47:10 +0700

    add meet out team

--- src/app/img/index.js
+++ src/app/img/index.js
@@ -82,6 +82,10 @@ import team1 from "./team-1.jpg";
 import team2 from "./team-2.jpg";
 import team3 from "./team-3.jpg";
 import team4 from "./team-4.jpg";
+import team5 from "./team-5.jpg";
+import team6 from "./team-6.jpg";
+import team7 from "./team-7.jpg";
+import team8 from "./team-8.jpg";
 
 import user from "./user.png";
 import sliderimg from "./sliderimg.jpg";
@@ -163,6 +167,10 @@ export default {
   team2,
   team3,
   team4,
+  team5,
+  team6,
+  team7,
+  team8,
   user,
   sliderimg,
 };
---
+++ src/app/team/page.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Banner from "./Banner/Banner";
+import OurTeam from "./OurTeam/OurTeam";
+import { Footer, FAQ, ChefTeam } from "../components/componentsindex";
+import { Brand, Reserve, Daily, Card } from "../components/componentsindex";
+import Style from "../styles/team.module.css"; // tạo file nếu muốn style toàn trang
+
+const page = () => {
+    return (
+        <div className={Style.teamPage}>
+            <Banner />
+            <OurTeam />
+            <FAQ/>
+            <Reserve/>
+            <Footer />
+        </div>
+    );
+};
+
+export default page;
